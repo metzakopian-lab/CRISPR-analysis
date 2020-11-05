726c142edc2b20f744825d599058e937e878d339
Nikos Patikas <nik.patik@gmail.com>
2020-11-05 18:40:19 +0000

    added typedef.h

---
+++ count-gRNAs/typedef.h
@@ -0,0 +1,8 @@
+#include <boost/algorithm/string/join.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string.hpp>
+
+
+using boost::algorithm::join;
+using boost::algorithm::split;
+using boost::is_any_of;
